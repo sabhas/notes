343ec61d1057a97a0b6c93cfa2e1c49262a4d5ae
Sabir Hassan <sabirbk06@gmail.com>
2022-12-02 00:30:29 +0500

    add basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from "react-router-dom"
+
 function App() {
-  return <div className="App">Hello World</div>
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Note List</h1>} />
+        <Route path="/new" element={<h1>New Note</h1>} />
+        <Route path="/:id" element={<h1>Note Layout</h1>}>
+          <Route index element={<h2>Note</h2>} />
+          <Route path="edit" element={<h2>Edit Note</h2>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  )
 }
 
 export default App
